6cf16c3476e56b38edefeb67189a80c947252835
Tung T.Q <tqtung@gmail.com>
2015-04-23 23:51:16 +0900

    initial commit

--- main.py
+++ main.py
@@ -1,15 +1,57 @@
 import argparse
+import os
+"""import datetime
+import re
+"""
+class Phorg:
+    IMAGE_EXTS = ['.jpg', '.gif', '.tiff', '.png']
+    def __init__(self, args):
+        self.__args = args
+        self.__file_list = []
+    def __process_file(self, afile):
+        file_path = os.path.abspath(afile)
+        file_name = os.path.split(file_path)[-1]
+        file_ext = os.path.splitext(file_name)[-1]
+        if file_ext not in Phorg.IMAGE_EXTS:
+            return self.__process_unexpected_file(afile)
+        print file_name, file_ext
+    def __process_unexpected_file(self, afile):
+        print afile, "is not a photo"
+    def process(self):
+        if not os.path.exists(self.__args.dest_dir) :
+            os.makedirs(self.__args.dest_dir)
 
-parser = argparse.ArgumentParser(description="A simple photo organizer")
-parser.add_argument("src_dir", help="photo directory")
-parser.add_argument("dest_dir", help="target directory")
-parser.add_argument("-r", "--recursive", action="store_true", dest="recursive", default=True, help="process folders recursively")
-parser.add_argument("-v", "--verbose", help="increase verbosity", action="store_true")
-parser.add_argument("-d", "--dry", dest="dry", action="store_true", help="dry run")
-args = parser.parse_args()
+        dir_list = [self.__args.src_dir]
+        self.__file_list = []
+        while len(dir_list)>0:
+            cur_dir = dir_list.pop()
+            for afile in os.listdir(cur_dir):
+                afile = os.path.join(cur_dir, afile)
+                if os.path.isfile(afile):
+                    self.__process_file(afile)
+                elif self.__args.recursive and os.path.isdir(afile):
+                    dir_list.append(afile)
 
-print "source folder = ", args.src_dir
-print "target folder = ", args.dest_dir
-print "is recursive = ", args.recursive
-print "is verbose = ", args.verbose
-print "is dry run = ", args.dry
+
+def main():
+    parser = argparse.ArgumentParser(description="A simple photo organizer")
+    parser.add_argument("src_dir", help="photo directory")
+    parser.add_argument("dest_dir", help="target directory")
+    parser.add_argument("-r", "--recursive", action="store_true", dest="recursive", default=False, help="process folders recursively")
+    parser.add_argument("-v", "--verbose", help="increase verbosity", action="store_true")
+    parser.add_argument("-d", "--dry", dest="dry", action="store_true", help="dry run")
+    parser.add_argument("-c", "--copy", dest="copy", help="copy photos instead of moving", action="store_true")
+    args = parser.parse_args()
+
+    print "source folder = ", args.src_dir
+    print "target folder = ", args.dest_dir
+    print "is recursive = ", args.recursive
+    print "is verbose = ", args.verbose
+    print "is dry run = ", args.dry
+    phorg = Phorg(args)
+    phorg.process()
+
+
+if __name__ == "__main__" : main()
+
+
--- phorg.py
+++ phorg.py
@@ -11,71 +11,74 @@ class PhotoData(object):
         self.__file_size = 0
         self.__datetime = None
         self.__camera = "unknown"
+    def __str__(self):
+        return self.__datetime.strftime("%Y-%m-%d_%H:%M:%S")
+    @property
+    def datetime(self):
+        """get the time when the photo was created"""
+        return self.__datetime
 
     def read_photo(self, file_path):
         self.__file_path = os.path.abspath(file_path)
-        self.__file_name, ext = os.path.split(self.__file_path)
-        self.__file_size = os.path.get_size(self.__file_path)
-        if ext in PhotoData.IMAGE_EXTS:
-            self.__get_date_time()
-    def __get_date_time(self):
-        exif = Metadata(self.__file_path)
-        if exif.has_tag("Exif.Image.DateTime"):
-            time_str = exif.get_tag_string("Exif.Image.DateTime")
-        elif exif.has_tag("Exif.Photo.DataTiimeOriginal"):
-            time_str = exif.get_tag_string("Exif.Photo.DateTimeOriginal")
+        self.__file_name = os.path.split(self.__file_path)[1]
+        ext = os.path.splitext(self.__file_name)[1]
+        self.__file_size = os.path.getsize(self.__file_path)
 
-        self.__datetime = datetime.strptime(time_str, "%Y:%m:%d %H:%M:%S")
+        if not ext in PhotoData.IMAGE_EXTS:
+            return False
+        if not self.__get_datetime_from_exif():
+            return self.__get_datetime_from_name()
+        else:
+            return True
+
+    def __get_datetime_from_exif(self):
+        try:
+            exif = Metadata(self.__file_path)
+            time_str = exif.get_tag_string("Exif.Image.DateTime")
+            if len(time_str) == 0:
+                time_str = exif.get_tag("Exif.Photo.DataTiimeOriginal")
+            self.__datetime = datetime.strptime(time_str, "%Y:%m:%d %H:%M:%S")
+            return True
+        except:
+            return False
+    def __get_datetime_from_name(self):
+        print "Trying to get datetime from %s" % self.__file_name
+        return False
 
 class Phorg :
-    PHOTO_EXTS = ['.jpg', '.gif', '.png', '.tiff']
     def __init__(self):
-        self.input_dir = "."
-        self.output_dir = "outputs"
-        self.auto_rename = True
-        self.grouping = True
-        self.file_dict = {}
+        self.__input_dir = "."
+        self.__output_dir = "outputs"
+        self.__auto_rename = True
+        self.__grouping = True
+        self.__file_dict = {}
 
     def readArgs(self, args) :
-        self.auto_rename = True
+        self.__auto_rename = True
 
     def process(self):
         self.__create_output_dir()
-        file_list = [self.input_dir]
-        while len(self.file_list) > 0 :
+        file_list = [self.__input_dir]
+        while len(file_list) > 0 :
             current_file = file_list.pop()
             if os.path.isdir(current_file) :
                 for file_path in os.listdir(current_file) :
                     file_list.append(os.path.join(current_file, file_path))
-            if os.path.isfile(current_file) :
+            elif os.path.isfile(current_file) :
                 self.__process_file(current_file)
-        if (self.grouping) :
+        if (self.__grouping) :
             self.__group()
-        print self.file_dict
+        print self.__file_dict
 
-    def __process_file(self, file_path) :
-        if self.__is_photo(file_path) :
-            self.__process_photo(file_path)
+    def __process_file(self, file_path):
+        photo = PhotoData()
+        if photo.read_photo(file_path):
+            self.__file_dict[photo.datetime] = photo
 
-    def __process_photo(self, file_path) :
-        exif = Metadata(file_path)
-        if exif.has_tag("Exif.Image.DateTime"):
-            time_str = exif.get_tag_string("Exif.Image.DateTime")
-        elif exif.has_tag("Exif.Photo.DataTiimeOriginal"):
-            time_str = exif.get_tag_string("Exif.Photo.DateTimeOriginal")
-
-        created_time = datetime.strptime(time_str, "%Y:%m:%d %H:%M:%S")
-        self.file_dict[created_time] = file_path
-        #print exif.get_tags()
-
-    def __is_photo(self, file_path):
-        name, ext = os.path.splitext(file_path)
-        if ext in Phorg.PHOTO_EXTS :
-            return True
-        return False
     def __create_output_dir(self) :
-        if not os.path.exists(self.output_dir) :
-            os.makedirs(self.output_dir)
+        if not os.path.exists(self.__output_dir) :
+            os.makedirs(self.__output_dir)
+
     def __group(self) :
         print "Do grouping"
 
